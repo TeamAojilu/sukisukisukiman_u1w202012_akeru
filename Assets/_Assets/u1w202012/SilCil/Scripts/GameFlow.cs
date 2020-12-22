e65f37cca0597ea64b689fd738e308030600360e
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-22 20:44:15 +0900

    Add: GameFlow
    とりあえず、10秒待つだけのゲームができた。

---
+++ Assets/_Assets/u1w202012/SilCil/Scripts/GameFlow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using SilCilSystem.Variables;
+using SilCilSystem.Singletons;
+
+namespace Unity1Week202012
+{
+    public class GameFlow : MonoBehaviour
+    {
+        [Header("Variables")]
+        [SerializeField] private VariableBool m_isPlaying = default;
+        [SerializeField] private VariableInt m_score = default;
+        [SerializeField] private VariableFloat m_time = default;
+
+        [Header("Initialize")]
+        [SerializeField] private float m_timeLimit = 30f;
+
+        [Header("Next Scenes")]
+        [SerializeField] private string m_nextScene = "Main";
+
+        private IEnumerator Start()
+        {
+            InitializeVariables();
+            yield return SceneLoader.WaitLoading;
+
+            m_isPlaying.Value = true;
+            yield return new WaitWhile(() => m_isPlaying);
+
+            SceneLoader.LoadScene(m_nextScene);
+        }
+
+        private void InitializeVariables()
+        {
+            m_isPlaying.Value = false;
+            m_score.Value = 0;
+            m_time.Value = m_timeLimit;
+        }
+    }
+}
