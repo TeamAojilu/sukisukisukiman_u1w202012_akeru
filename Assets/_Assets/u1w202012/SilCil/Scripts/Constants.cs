0661d982d505cde53997bbc0375388dd5ccb90dd
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-25 01:05:09 +0900

    Merge issues

--- Assets/_Assets/u1w202012/SilCil/Scripts/Constants.cs
+++ Assets/_Assets/u1w202012/SilCil/Scripts/Constants.cs
@@ -4,7 +4,7 @@ namespace Unity1Week202012
 {
     public static class Constants
     {
-        private static readonly string[] m_colors = new string[] { "red", "blue", "green", "black" };
+        private static readonly string[] m_colors = new string[] { "red", "blue", "green", "yellow" };
         private static readonly string[] m_shapes = new string[] { "0", "1", "2", "3" };
         public static IReadOnlyList<string> ColorNames => m_colors;
         public static IReadOnlyList<string> ShapeNames => m_shapes;
