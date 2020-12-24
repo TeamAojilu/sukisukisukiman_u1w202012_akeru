676f0fdf59b28e911dfe7f303372ae57886bcb08
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-12-24 23:24:00 +0900

    add:既定のprefab

---
+++ Assets/_Assets/u1w202012/Issues/Issue#40/Others/CanvasCombinationViewer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+using TMPro;
+namespace Unity1Week202012
+{
+    public class CanvasCombinationViewer : MonoBehaviour, ICombinationsViewer
+    {
+        [SerializeField] private TextMeshProUGUI m_textPrefab;
+        [SerializeField] private RectTransform m_parent;
+
+        List<GameObject> m_textObjList = new List<GameObject>();
+        private void Start()
+        {
+            Services.CombinationsViewer = this;
+        }
+
+        public void Add(CombinationData combination, int count)
+        {
+            var obj = Instantiate(m_textPrefab, m_parent);
+            m_textObjList.Add(obj.gameObject);
+            obj.text = $"{combination.m_displayText}: {combination.m_score} x {count}";
+        }
+
+        public void Clear()
+        {
+            m_textObjList.ForEach(x => Destroy(x));
+            m_textObjList = new List<GameObject>();
+        }
+    }
+}
