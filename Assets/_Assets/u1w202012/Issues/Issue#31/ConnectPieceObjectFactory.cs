18b2f86764ff01a2403f32b5b5f09ed548b29716
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-12-24 16:31:23 +0900

    Add:IPieceObjectFactoryを継承したクラスを作成

---
+++ Assets/_Assets/u1w202012/Issues/Issue#31/ConnectPieceObjectFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012
+{
+    public class ConnectPieceObjectFactory : MonoBehaviour, IPieceObjectFactory
+    {
+        [Serializable]
+        private class BlockInfo
+        {
+            public string m_colorName = Constants.ColorNames[0];
+            public string m_shapeName = Constants.ShapeNames[0];
+            public GameObject m_prefab = default;
+        }
+
+        [SerializeField] private Piece m_parentPrefab = default;
+        [SerializeField] private BlockInfo[] m_blockPrefabs = default;
+        [SerializeField] private float m_blockSize = 1f;
+
+        private void Awake()
+        {
+            Services.PieceObjectFactory = this;
+        }
+
+        public Piece Create(PieceData pieceData)
+        {
+            if (pieceData == null) return null;
+
+            var blockPrefab = m_blockPrefabs.FirstOrDefault(b => b.m_colorName == pieceData.m_color && b.m_shapeName == pieceData.m_shape);
+            if (blockPrefab?.m_prefab == null) return null;
+
+            var parent = Instantiate(m_parentPrefab, Vector3.zero, Quaternion.identity);
+            parent.m_pieceData = pieceData;
+
+            foreach (var pos in pieceData.m_positions)
+            {
+                var position = new Vector3(pos.x, pos.y, 0f) * m_blockSize;
+                var block = Instantiate(blockPrefab.m_prefab, position, Quaternion.identity, parent.transform);
+            }
+
+            return parent;
+        }
+    }
+}
