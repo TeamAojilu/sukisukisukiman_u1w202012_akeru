209f36910d857718ddb42d09fcae0ccaddbe4000
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-12-25 22:28:54 +0900

    add:Testクラス

---
+++ Assets/_Assets/u1w202012/Issues/Issue#79/GameDataNCMB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+using NCMB;
+
+namespace Unity1Week202012
+{
+    [Serializable]
+    public class NCMBPlayerData
+    {
+        [SerializeField] public int m_maxScore;
+    }
+
+    public class GameDataNCMB : MonoBehaviour
+    {
+        string m_NCMBobject_ClassName = "PlayerData";
+        string m_NCMBobject_JsonName = "json";
+
+        public void Save(NCMBPlayerData data)
+        {
+            NCMBObject testClass = new NCMBObject(m_NCMBobject_ClassName);
+            testClass["json"] = JsonUtility.ToJson(data);
+            
+            testClass.SaveAsync();
+        }
+
+        public IEnumerable Load()
+        {
+            yield return null;
+        }
+    }
+}
