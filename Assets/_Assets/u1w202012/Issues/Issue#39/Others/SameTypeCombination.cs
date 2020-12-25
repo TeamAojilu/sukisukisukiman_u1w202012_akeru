63e287f7f1008bacd7a10c5e4b64ff6818dee432
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-12-25 17:20:06 +0900

    add:タイプのコンボ判定を実装

---
+++ Assets/_Assets/u1w202012/Issues/Issue#39/Others/SameTypeCombination.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012
+{
+    public class SameTypeCombination : MonoBehaviour,ICombination
+    {
+        private const int MinCount = 2;
+        private const string KeyFormat = "SameType_{0}_{1}";
+        private HashSet<PieceData> m_checked = new HashSet<PieceData>();
+
+        private void Start()
+        {
+            Services.Combinations.Add(this);
+        }
+
+        public void SetupBeforeEvaluate()
+        {
+            m_checked.Clear();
+        }
+
+        public string Evaluate(PieceData pieceData)
+        {
+            if (pieceData == null) return null;
+            if (m_checked.Contains(pieceData)) return null;
+
+            string shape = pieceData.m_shape;
+            HashSet<PieceData> group = CombinationUtility.GetGroup(pieceData, x => x.m_shape == shape);
+            m_checked.UnionWith(group);
+
+            if (group.Count >= MinCount)
+            {
+                return string.Format(KeyFormat, shape, group.Count);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
