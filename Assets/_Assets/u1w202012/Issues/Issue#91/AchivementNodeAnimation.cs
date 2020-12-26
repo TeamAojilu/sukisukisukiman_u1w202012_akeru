5ab0f6844012d7209cd7701c37b8abdad39d71e9
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-12-26 22:35:25 +0900

    add:いろいろ

---
+++ Assets/_Assets/u1w202012/Issues/Issue#91/AchivementNodeAnimation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+using SilCilSystem.Math;
+
+namespace Unity1Week202012
+{
+    public class AchivementNodeAnimation : MonoBehaviour
+    {
+        [SerializeField] private Vector3 m_start_slideIn = new Vector3(0f, 0f, 1f);
+        [SerializeField] private Vector3 m_end_slideIn = new Vector3(1f, 1f, 1f);
+        [SerializeField] private Vector3 m_start_moveLine = new Vector3(0f, 0f, 1f);
+        [SerializeField] private Vector3 m_end_moveLine = new Vector3(1f, 1f, 1f);
+        [SerializeField] private InterpolationCurve m_curve = default;
+        [SerializeField] private float m_animationSpeed = 1f;
+
+        RectTransform rectTr;
+
+        public bool NowAnimation { get; private set; } = false;
+
+
+        public IEnumerator SlideIn()
+        {
+            rectTr = GetComponent<RectTransform>();
+            while (NowAnimation)
+            {
+                yield return null;
+            }
+            NowAnimation = true;
+            rectTr.localPosition = m_start_slideIn;
+
+            float t = 0f;
+            while (t < 1f)
+            {
+                t += Time.deltaTime * m_animationSpeed;
+                rectTr.localPosition = Vector3.LerpUnclamped(m_start_slideIn, m_end_slideIn, m_curve.Evaluate(t));
+                yield return null;
+            }
+
+            rectTr.localPosition = m_end_slideIn;
+
+            NowAnimation = false;
+        }
+
+        public IEnumerator MoveUp()
+        {
+            rectTr = GetComponent<RectTransform>();
+            while (NowAnimation)
+            {
+                yield return null;
+            }
+            NowAnimation = true;
+            rectTr.localPosition = m_start_moveLine;
+
+            float t = 0f;
+            while (t < 1f)
+            {
+                t += Time.deltaTime * m_animationSpeed;
+                rectTr.localPosition = Vector3.LerpUnclamped(m_start_slideIn, m_end_slideIn, m_curve.Evaluate(t));
+                yield return null;
+            }
+
+            rectTr.localPosition = m_end_moveLine;
+
+            NowAnimation = false;
+        }
+    }
+}
---
+++ Assets/_Assets/u1w202012/Issues/Issue#91/AnimationAcievementNode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+using SilCilSystem.Components.Views;
+
+namespace Unity1Week202012
+{
+    public class AnimationAcievementNode : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_displayText = default;
+        [SerializeField] private float m_time = 1f;
+
+        [SerializeField] AchivementNodeAnimation anim;
+        public Transform CashedTransform { get => m_transform = m_transform ?? transform; }
+        private Transform m_transform = default;
+
+        private IDisplayText DisplayText { get => m_display = m_display ?? m_displayText.GetTextComponent(); }
+        private IDisplayText m_display = default;
+
+
+        private Action m_closeAction;
+
+        public bool m_animationNow { get { return m_animStack.Count > 0; } }
+        Queue<string> m_animStack = new Queue<string>();
+        public IEnumerator ShowCoroutine(AchievementData data, Action callback)
+        {
+            m_animStack.Enqueue("a");
+            m_closeAction = callback;
+
+            DisplayText?.SetText(data.m_displayName);
+            yield return anim.SlideIn();
+            m_startTime = Time.time;
+
+            m_animStack.Dequeue();
+            //yield return new WaitForSeconds(m_time);
+
+            //callback?.Invoke();
+        }
+
+        public IEnumerator MoveUp()
+        {
+            m_animStack.Enqueue("a");
+            yield return anim.MoveUp();
+            m_startTime = Time.time;
+            m_animStack.Dequeue();
+        }
+
+        float m_startTime;
+        private void Update()
+        {
+            if (m_animationNow) return;
+            if (m_startTime + m_time > Time.time)
+            {
+                m_closeAction.Invoke();
+            }
+        }
+    }
+}
