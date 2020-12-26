d12d181aef9cb228c1f42b4737717e5cd86d3efc
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-27 06:54:46 +0900

    Add: Title

--- Assets/_Assets/u1w202012/Issues/Issue#119/Scripts/DisplayScoreRank.cs
+++ Assets/_Assets/u1w202012/Issues/Issue#119/Scripts/DisplayScoreRank.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
 using SilCilSystem.Variables;
 using SilCilSystem.Math;
