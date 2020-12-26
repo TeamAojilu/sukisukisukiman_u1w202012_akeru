1a965e2c05c7ac07bd6eea8c6a4ae0dc4d93d03f
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-27 03:39:11 +0900

    Add: BonusEffect

---
+++ Assets/_Assets/u1w202012/Issues/Issue#111/Scripts/BonusCharacter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using SilCilSystem.Math;
+
+namespace Unity1Week202012.Issue111
+{
+    public class BonusCharacter : MonoBehaviour
+    {
+        [Header("Scale")]
+        [SerializeField] private Vector3 m_start = new Vector3(0f, 0f, 1f);
+        [SerializeField] private Vector3 m_end = new Vector3(1f, 1f, 1f);
+        [SerializeField] private InterpolationCurve m_curve = default;
+
+        [Header("positionY")]
+        [SerializeField] private Vector2 m_moveRangeY = new Vector2(0f, 1f);
+        [SerializeField] private InterpolationCurve m_movingCurve = default;
+
+        [SerializeField] private float m_animationSpeed = 1f;
+        [SerializeField] private Vector2 m_beforeNoiseRenge;
+
+        private Transform m_transform = default;
+
+        public IEnumerator PlayEffect(Vector2Int position, float delay, Action callback = null)
+        {
+            m_transform = m_transform ?? transform;
+
+            m_transform.position = new Vector3(position.x, position.y, 0f);
+            var startPos = m_transform.position + Vector3.up * m_moveRangeY.x;
+            var endPos = m_transform.position + Vector3.up * m_moveRangeY.y;
+
+            m_transform.localScale = m_start;
+            yield return new WaitForSeconds(delay);
+
+            //終わりを合わせるための再生速度の計算
+            var notWaitAnimTime = 1 / m_animationSpeed;
+            var calcedAnimTime = notWaitAnimTime - delay;
+            var calcedAnimSpeedRate = 1 / calcedAnimTime;
+
+            float t = 0f;
+            while (t < 1f)
+            {
+                t += Time.deltaTime * calcedAnimSpeedRate;
+                m_transform.position = Vector3.LerpUnclamped(startPos, endPos, m_movingCurve.Evaluate(t));
+                m_transform.localScale = Vector3.LerpUnclamped(m_start, m_end, m_curve.Evaluate(t));
+                yield return null;
+            }
+
+            m_transform.localScale = m_end;
+            callback?.Invoke();
+        }
+    }
+}
---
+++ Assets/_Assets/u1w202012/Issues/Issue#111/Scripts/ScoreCalculatorsSetter.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+using System.Collections;
+using System.Linq;
+
+namespace Unity1Week202012.Issue111
+{
+    public class ScoreCalculatorsSetter : MonoBehaviour, IBonusEffect
+    {
+        [Header("Score")]
+        [SerializeField] private GameEventFloatListener[] m_onFloatChanged = default;
+        [SerializeField] private ReadonlyPropertyFloat m_scoreMultiply = new ReadonlyPropertyFloat(100f);
+        [SerializeField] private ReadonlyPropertyFloat m_bonusMultiply = new ReadonlyPropertyFloat(50f);
+
+        [Header("BonusEffect")]
+        [SerializeField] private BonusCharacter m_bonusCharacterPrefab = default;
+        [SerializeField] private Vector2 m_delayRange = new Vector2(0f, 0.5f);
+        [SerializeField] private AudioSource[] m_audioSources = default;
+        [SerializeField] private float m_waitTimeBefore = 0.5f;
+        [SerializeField] private float m_waitTimeAfter = 0.5f;
+
+        private MaxSquareCalculator m_maxSquareCalculator = default;
+        private List<BonusCharacter> m_bonusCharacters = new List<BonusCharacter>();
+
+        private void Start()
+        {
+            SetCalculators();
+            foreach(var listen in m_onFloatChanged)
+            {
+                listen?.Subscribe(x => SetCalculators())?.DisposeOnDestroy(gameObject);
+            }
+
+            Services.BonusEffect = this;
+        }
+
+        private void SetCalculators()
+        {
+            Services.ScoreCalculator = new SummentionScore(GetScoreCalculators());
+            m_maxSquareCalculator = new MaxSquareCalculator();
+            Services.BonusCalculator = new BonusCalculator(m_maxSquareCalculator, (s, b) => s + b * m_bonusMultiply);
+        }
+
+        private IEnumerable<IScoreCalculator> GetScoreCalculators()
+        {
+            foreach (var color in Constants.ColorNames)
+            {
+                yield return new ScoreMultiplier(m_scoreMultiply, new SameColorAverageCalculator(color));
+            }
+            foreach(var shape in Constants.ShapeNames)
+            {
+                yield return new ScoreMultiplier(m_scoreMultiply, new SameShapeAverageCalculator(shape));
+            }
+        }
+
+        public IEnumerator BonusEffectCoroutine()
+        {
+            int count = 0;
+            int finishCount = 0;
+            for(int x = 0; x < m_maxSquareCalculator.MaxSize.x; x++)
+            {
+                for(int y = 0; y < m_maxSquareCalculator.MaxSize.y; y++)
+                {
+                    count++;
+                    var delay = Random.Range(m_delayRange.x, m_delayRange.y);
+                    var bonus = GetBonusCharacter();
+                    StartCoroutine(bonus.PlayEffect(m_maxSquareCalculator.MaxPosition + new Vector2Int(x, -y), delay, () => 
+                    {
+                        finishCount++;
+                    }));
+                    
+                    if(count < m_audioSources.Length)
+                    {
+                        m_audioSources[count].PlayDelayed(delay);
+                    }
+                }
+            }
+
+            while (count < finishCount) yield return null;
+
+            yield return new WaitForSeconds(m_waitTimeBefore);
+            m_bonusCharacters.ForEach(x => x.gameObject.SetActive(false));
+            yield return new WaitForSeconds(m_waitTimeAfter);
+        }
+
+        private BonusCharacter GetBonusCharacter()
+        {
+            var bonus = m_bonusCharacters.FirstOrDefault(x => !x.gameObject.activeSelf);
+            if(bonus == null)
+            {
+                bonus = Instantiate(m_bonusCharacterPrefab);
+                m_bonusCharacters.Add(bonus);
+            }
+            else
+            {
+                bonus.gameObject.SetActive(true);
+            }
+            return bonus;
+        }
+    }
+}
