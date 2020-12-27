139dc140d8158f777278d8cd7e300e13b659fa87
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-12-27 16:24:36 +0900

    Modify:実績確認画面

---
+++ Assets/_Assets/u1w202012/Issues/Issue#135/SerializeTextContent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity1Week202012.Aojilu.Title
+{
+    public class SerializeTextContent : MonoBehaviour, ITextContent
+    {
+        [SerializeField] Text targetText;
+        public void DisplayText(string text)
+        {
+            targetText.text = text;
+        }
+    }
+}
---
+++ Assets/_Assets/u1w202012/Issues/Issue#135/TextAchivementDataSuplier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012.Aojilu.Title
+{
+    public class TextAchivementDataSuplier : MonoBehaviour, IAchivementTextSupplier
+    {
+        private void Start()
+        {
+            AojiluService_Title.AchivementTextSupplier = this;
+        }
+
+        public Dictionary<string, bool> GetAchivementDataList()
+        {
+            var dataDictionary= AojiluService.DataSaver.PlaySaveData.AchivementDatas;
+            var result = new Dictionary<string, bool>();
+
+            AchievementTexts achi = new AchievementTexts();
+            foreach (var data in dataDictionary)
+            {
+                if (!achi.TryGetAchievementData(data.Key, out var achieveData)) continue;
+                result.Add(achieveData.m_displayName, dataDictionary[achieveData.m_id]);
+            }
+
+            return result;
+        }
+        
+        
+    }
+}
