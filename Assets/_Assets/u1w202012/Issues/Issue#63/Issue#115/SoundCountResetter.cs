f1f35e5aa486cd05db74e802609292dc77c1f500
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-27 17:05:59 +0900

    Add: Sound Count Limitation

---
+++ Assets/_Assets/u1w202012/Issues/Issue#63/Issue#115/SoundCountResetter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012
+{
+    public class SoundCountResetter : MonoBehaviour
+    {
+        [SerializeField] private GameEventBoolListener m_onIsPlayingChanged = default;
+        [SerializeField] private GameEventListener m_onSubmit = default;
+        [SerializeField] private VariableInt m_soundCount = default;
+
+        private void Start()
+        {
+            m_onSubmit?.Subscribe(ResetCount).DisposeOnDestroy(gameObject);
+            m_onIsPlayingChanged?.Subscribe(_ => ResetCount());
+        }
+
+        private void ResetCount()
+        {
+            m_soundCount.Value = 0;
+        }
+    }
+}
