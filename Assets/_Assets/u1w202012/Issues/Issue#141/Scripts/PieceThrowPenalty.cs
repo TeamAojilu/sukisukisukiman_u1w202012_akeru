08320967825a16138b58f2d7713682135a878082
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-27 17:31:53 +0900

    Add: LossTime

---
+++ Assets/_Assets/u1w202012/Issues/Issue#141/Scripts/PieceThrowPenalty.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012
+{
+    public class PieceThrowPenalty : MonoBehaviour
+    {
+        [SerializeField] private VariableFloat m_playTime = default;
+        [SerializeField] private ReadonlyPropertyFloat m_decrease = new ReadonlyPropertyFloat(5f);
+        [SerializeField] private GameEventListener m_OnPieceThrown = default;
+
+        private void Start()
+        {
+            m_OnPieceThrown?.Subscribe(() =>
+            {
+                m_playTime.Value -= m_decrease;
+            }).DisposeOnDestroy(gameObject);
+        }
+    }
+}
