06df1e9afae42c15d3320cc2d0842dcb3ec297f6
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-12-27 18:48:01 +0900

    Add:Title

---
+++ Assets/_Assets/u1w202012/Issues/Issue#109_title/Scripts/ConcreteInterface/DisplayRecordCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012.Aojilu.Title
+{
+    public class DisplayRecordCanvas : MonoBehaviour,I
+    {
+    }
+}
