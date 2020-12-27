653003bafa0a4e88c993276715c5c9a07a53c6c1
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-27 20:20:00 +0900

    Add

---
+++ Assets/_Assets/u1w202012/Issues/Issue#72_Tweet/EnableToPlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Variables;
+using UnityEngine.Events;
+
+namespace Unity1Week202012
+{
+    public class EnableToPlay : MonoBehaviour
+    {
+        public UnityEvent m_onEnable = default;
+
+        private void OnEnable()
+        {
+            m_onEnable.Invoke();
+        }
+    }
+}
