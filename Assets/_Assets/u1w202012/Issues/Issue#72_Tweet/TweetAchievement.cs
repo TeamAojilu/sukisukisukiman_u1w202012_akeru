5db97ef1c0634564da8138bbfd052c37f0e4d830
kugimasa <kugimania@gmail.com>
2020-12-27 19:58:36 +0900

    Force PUSH

---
+++ Assets/_Assets/u1w202012/Issues/Issue#72_Tweet/TweetAchievement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012
+{
+    public class TweetAchievement : MonoBehaviour
+    {
+        [SerializeField] private GameEventString m_onAchieved = default;
+        [SerializeField] private GameEventListener m_onTweeted = default;
+
+        private void Start()
+        {
+            m_onTweeted?.Subscribe(() => m_onAchieved?.Publish("press_tweet")).DisposeOnDestroy(gameObject);
+        }
+    }
+}
