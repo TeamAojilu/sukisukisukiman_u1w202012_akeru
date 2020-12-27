1319baa885f427692d66737b751d0a9c1e6a7d10
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-27 18:01:18 +0900

    Add: AchivementData

--- Assets/_Assets/u1w202012/Issues/Issue#141/Scripts/PieceThrowPenalty.cs
+++ Assets/_Assets/u1w202012/Issues/Issue#141/Scripts/PieceThrowPenalty.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using SilCilSystem.Variables;
+using System.Linq;
 
 namespace Unity1Week202012
 {
@@ -9,12 +10,20 @@ namespace Unity1Week202012
         [SerializeField] private ReadonlyPropertyFloat m_decrease = new ReadonlyPropertyFloat(5f);
         [SerializeField] private GameEventListener m_OnPieceThrown = default;
 
+        [SerializeField] private GameObject[] m_effectObjects = default;
+
         private void Start()
         {
             m_OnPieceThrown?.Subscribe(() =>
             {
                 m_playTime.Value -= m_decrease;
             }).DisposeOnDestroy(gameObject);
+        }
+
+        private void ShowEffect()
+        {
+            var first = m_effectObjects.FirstOrDefault(x => !x.activeSelf);
+            first.SetActive(true);
         }
     }
 }
