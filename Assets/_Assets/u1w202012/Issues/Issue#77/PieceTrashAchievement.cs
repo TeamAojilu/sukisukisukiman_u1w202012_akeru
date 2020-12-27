e4cee58867d87961799d08478aa189b145217970
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-27 19:00:47 +0900

    Change achievement

---
+++ Assets/_Assets/u1w202012/Issues/Issue#77/PieceTrashAchievement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using SilCilSystem.Variables;
+
+namespace Unity1Week202012
+{
+    public class PieceTrashAchievement : MonoBehaviour
+    {
+        [SerializeField] private GameEventString m_onAchived = default;
+        [SerializeField] private GameEventListener m_onPieceTrashed = default;
+
+        private void Start()
+        {
+            m_onPieceTrashed?.Subscribe(() => m_onAchived?.Publish("piece_trashed")).DisposeOnDestroy(gameObject);
+        }
+    }
+}
